80550178d657f9b0197e4d6decbb2b150770f338
BenjaminLavine01 <157385838+BenjaminLavine01@users.noreply.github.com>
2026-02-08 14:46:36 +0700

    4bug fixes

--- client/src/pages/Dashboard/CourseCatalog.js
+++ client/src/pages/Dashboard/CourseCatalog.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BookMarked, BookOpen as BookIcon } from 'lucide-react';
+import { BookOpen as BookIcon } from 'lucide-react';
 import api from '../../services/api';
 import './CourseCatalog.css';
 
